5186d0afed9459446bc4f8328b8b993ab561584b
Emirhan Uluksar <88688871+emirhanuluksar@users.noreply.github.com>
2022-11-02 21:16:20 +0300

    Update Program.cs

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
 
 namespace ClassMethodDemo
 {
@@ -6,7 +9,38 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager();
+            // ctrl + k --> ctrl + c comment
+            //Customer customer1 = new Customer
+            //{
+            //    Id = 1,
+            //    FirstName = "John",
+            //    LastName = "Doe"
+            //};
 
+            //Customer customer2 = new Customer
+            //{
+            //    Id = 2,
+            //    FirstName = "John2",
+            //    LastName = "Doe"
+            //};
+
+            // Another way
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "John";
+            customer1.LastName = "Doe";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "John2";
+            customer2.LastName = "Doe";
+
+            customerManager.Add(customer1);
+            customerManager.Delete(customer1);
+            customerManager.Update(customer1);
+            customerManager.customerList(customer2);
+            
         }
     }
 }
