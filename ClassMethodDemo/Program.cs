111a98981ebe766da74672e1925437b5b7aa52a8
Emirhan Uluksar <88688871+emirhanuluksar@users.noreply.github.com>
2022-11-02 20:39:46 +0300

    Update Program.cs

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,11 +6,7 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            /*
-                Customer adlı bir class oluşturucam ve bu classa bazı özellikler vericem.
-                Id ad soyad vs
-                CustomerManager sınıfı oluşturup bu sınıfta customer ile alakalı ekleme silme listeleme gibi metotlar yazıcam
-             */
+
         }
     }
 }
